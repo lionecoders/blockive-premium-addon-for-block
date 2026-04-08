7b71765b9b6c57ac2cf5b3b28ef534d599337fae
Mandeep Singh <156427950+deep7197@users.noreply.github.com>
2026-04-08 21:38:51 +0530

    add font awosom and category in block editor

--- lc-block-widgets.php
+++ lc-block-widgets.php
@@ -24,7 +24,25 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
  * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
  */
+function create_block_lc_block_widgets_categories( $categories ) {
+	return array_merge(
+		array(
+			array(
+				'slug'  => 'lc-widgets',
+				'title' => esc_html__( 'LC Widgets', 'lc-block-widgets' ),
+			),
+		),
+		$categories
+	);
+}
+add_filter( 'block_categories_all', 'create_block_lc_block_widgets_categories', 10, 2 );
+
 function create_block_lc_block_widgets_block_init() {
 	wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
 }
 add_action( 'init', 'create_block_lc_block_widgets_block_init' );
+
+function create_block_lc_block_widgets_enqueue_assets() {
+	wp_enqueue_style( 'lc-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array(), '6.4.0' );
+}
+add_action( 'enqueue_block_assets', 'create_block_lc_block_widgets_enqueue_assets' );
