e107ab691dfba79675d84f61bc9e9bc7a0de388d
Mandeep Singh <156427950+deep7197@users.noreply.github.com>
2026-04-12 22:05:44 +0530

    fix boafer issue

--- build/accordion/index.asset.php
+++ build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1feffc8d1ebd5eca00ad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b989c181760ef279917');
