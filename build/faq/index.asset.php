03b2263d8d0ee6b18ed96f879dd0632858510618
Deep <tayalvishali73@gmail.com>
2026-04-12 22:10:02 +0530

    Revert "fix boafer issue"
    This reverts commit e107ab691dfba79675d84f61bc9e9bc7a0de388d.

--- build/faq/index.asset.php
+++ build/faq/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b18f0ba2a8cc82557aaf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c084d1d730a2135b22a3');
