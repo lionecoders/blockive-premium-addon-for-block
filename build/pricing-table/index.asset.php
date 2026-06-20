13eafb05796aa291010e096e048cfd0bcda8b36f
Deep <tayalvishali73@gmail.com>
2026-06-20 16:54:46 +0530

    remove birder from main container

--- build/pricing-table/index.asset.php
+++ build/pricing-table/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '94ed423672153aa2835a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2de9ef826277b3951bbb');
